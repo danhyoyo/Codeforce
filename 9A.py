4d3997ed5fa8d4795e9194eede32dd10c05e7b17
danhyoyo <123ducanhanhduc123@gmail.com>
2025-02-11 08:52:48 +0700

    9A

---
+++ 9A.py
@@ -0,0 +1,14 @@
+a, b = map(int,input().split())
+c = max(a,b)
+if c == 1:
+    print("1/1")
+if c == 2:
+    print("5/6")
+if c == 3:
+    print("2/3")
+if c == 4:
+    print("1/2")
+if c == 5:
+    print("1/3")
+if c == 6:
+    print("1/6")
